cd74a729ca05d832fabd01c7c1c64a1897eac86a
phongnguyen <phongnguyen25000@gmail.com>
2025-11-06 15:11:02 +0100

    correction DELETE /cities/:zipcode

--- api.test.ts
+++ api.test.ts
@@ -30,21 +30,34 @@ describe("API Meteo", () => {
       },
     ]);
   });
-  it('GET /cities/:zipCode récupère une ville par son code postal', async () => {
-    const res = await request(app).get('/cities/21000');
+  it("GET /cities/:zipCode récupère une ville par son code postal", async () => {
+    const res = await request(app).get("/cities/21000");
     expect(res.status).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body).toEqual([
       {
-        zipCode: '21000',
-        name: 'Dijon',
+        zipCode: "21000",
+        name: "Dijon",
       },
     ]);
   });
 
-  it('DELETE cities/:zipCode supprime une ville par son code postal', async () => {
-    const res = await request(app).delete('/cities/25000');
+  it("DELETE cities/:zipCode supprime une ville par son code postal", async () => {
+    const res = await request(app).delete("/cities/25000");
     expect(res.status).toBe(204);
   });
 
+//   it("PUT /cities/:zipCode met à jour le nom d'une ville par son code postal", async () => {
+//     const res = await request(app)
+//       .put("/cities/21000")
+//       .send({ name: "New Dijon" });
+//     expect(res.status).toBe(201);
+//     expect(Array.isArray(res.body)).toBe(true);
+//     expect(res.body).toEqual([
+//       {
+//         zipCode: "21000",
+//         name: "New Dijon",
+//       },
+//     ]);
+//   });
 });
--- server.ts
+++ server.ts
@@ -22,12 +22,26 @@ app.delete("/cities/:zipCode", (req, res) => {
   const zipCode = req.params.zipCode;
   const city = cities.findIndex((c) => c.zipCode === zipCode);
 
-  if (!city) {
+  if (city === -1) {
     return res.status(404).json({ error: "City not found" });
   }
   cities.splice(city, 1);
   fs.writeFileSync("cities.json", JSON.stringify(cities, null, 2));
   res.status(204).json();
+
+  
 });
+
+// app.put("/cities/:zipCode", (req, res) => {
+//   const zipCode = req.params.zipCode;
+//   const city = cities.findIndex((c) => c.zipCode === zipCode);
+
+//   if (!city) {
+//     return res.status(404).json({ error: "City not found" });
+//   }
+//   cities[city].name = req.body.name;
+//   fs.writeFileSync("cities.json", JSON.stringify(cities, null, 2));
+//   res.status(201).json(cities);
+// });
 
 export default app;
